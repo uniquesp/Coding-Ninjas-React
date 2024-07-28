8f38f37ee3eedf8c97b4576fdc128e4fc7a901d9
Sakshi Pharande <71183249+uniquesp@users.noreply.github.com>
2024-07-28 22:49:20 +0530

    Add files via upload

---
+++ Week4/causing-side-effects/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ComponentA from "./Components/ComponentA";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <>
+        <ComponentA />
+      </>
+    );
+  }
+}
+
+export default App;
